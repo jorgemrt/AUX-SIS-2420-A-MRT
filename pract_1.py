15d7bebf6bdc89b0733cb61797b6f6d33ae98eb6
jorgemrt <jorge.martinez@sistemas.edu.bo>
2025-03-08 18:48:42 -0400

    suma,resta,divicion,multplicacion

---
+++ pract_1.py
@@ -0,0 +1,13 @@
+#1_clas_aux_mrt
+#Ejercicio #1
+print ("inse dos numeros ")
+n1 = input("perimer numero :")
+n2 = input("segundo nnumero ") 
+suma = int(n1)+ int (n2)
+resta = int(n1)-int (n2)
+multi = int(n1) * int (n2)
+divi = int(n1)/ int (n2)
+print("la suma es :", suma )
+print("la resta  es :", resta )
+print("la multiplicacion es :", multi )
+print("la  divicion es :", divi )
