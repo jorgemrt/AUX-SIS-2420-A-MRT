c2d692fe5052e893941b0146dde0199015c10640
jorgemrt <jorge.martinez@sistemas.edu.bo>
2025-03-08 20:01:28 -0400

    agregando par e impar

--- pract_1.py
+++ pract_1.py
@@ -2,7 +2,9 @@
 #Ejercicio #1
 print ("inse dos numeros ")
 n1 = input("perimer numero :")
-n2 = input("segundo nnumero ") 
+n2 = input("segundo nnumero ")
+n1 =int(n1)
+n2 =int(n2)
 suma = int(n1)+ int (n2)
 resta = int(n1)-int (n2)
 multi = int(n1) * int (n2)
@@ -11,3 +13,13 @@ print("la suma es :", suma )
 print("la resta  es :", resta )
 print("la multiplicacion es :", multi )
 print("la  divicion es :", divi )
+# VERIFICANDO SI ES PAR O IMPAR
+veridig1 = n1 % 2 == 0
+veridig2 = n2 % 2 == 0
+print("El primer dígito es par:", veridig1)
+print("El segundo dígito es par:", veridig2)
+
+
+
+
+
